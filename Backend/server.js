f3710e16b38de4e09c188eda3e612f6bbc0dd047
Santosh-7124 <sunny962002@gmail.com>
2025-11-22 20:33:55 +0530

    Final fix

--- Backend/server.js
+++ Backend/server.js
@@ -25,15 +25,15 @@ app.get("/", (req, res) => {
 
 // Gmail Transporter
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS, // must be app password!
+        pass: process.env.SMTP_PASS,
     },
-    tls: {
-        rejectUnauthorized: false,
-    }
 });
+
 
 app.post("/api/contact", async (req, res) => {
     try {
