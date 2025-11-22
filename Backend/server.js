78ccdff1cb4c7da5c37b3fbc910848e531027c56
Santosh-7124 <sunny962002@gmail.com>
2025-11-22 20:23:31 +0530

    Error fix

--- Backend/server.js
+++ Backend/server.js
@@ -10,6 +10,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+    res.send("Backend is running");
+});
+
 // Gmail transporter
 const transporter = nodemailer.createTransport({
     service: "gmail",
